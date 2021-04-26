6b86091d8853b22c54c7e2cc3e555e1e84604f23
Johannes Frohnhofen <jf@daedalean.ai>
2021-04-26 16:21:32 +0200

    doc -> docs

--- gen-docs.sh
+++ gen-docs.sh
@@ -2,12 +2,13 @@
 
 # Generates Go documentation and exports it as static HTML.
 
-rm -rf doc
+rm -rf docs
 ln -s RULES/ src
 godoc -goroot=`pwd` -http=:8080 &
 GODOCPID=$!
 sleep 1
 wget -r -np -N -E -p -k -e robots=off http://localhost:8080/pkg/
-mv localhost:8080/ doc/
+mv localhost:8080/ docs/
+echo "<head><meta http-equiv=\"refresh\" content=\"0; URL=pkg/\" /></head>" > docs/index.html
 kill -9 $GODOCPID
 rm src
