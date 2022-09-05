40a53c3cab2aa3e70794bd7ce0aee3afca1c2258
Javier Alvarez Garcia <ja@daedalean.ai>
2022-09-05 14:55:32 +0200

    Run gofmt on sources

--- RULES/core/main.go
+++ RULES/core/main.go
@@ -15,7 +15,7 @@ type targetInfo struct {
 	Description string
 	Runnable    bool
 	Testable    bool
-	Report		bool
+	Report      bool
 }
 
 type generatorInput struct {
@@ -29,7 +29,7 @@ type generatorInput struct {
 	RunArgs         []string
 	TestArgs        []string
 	Layout          string
-	SelectedTargets        []string
+	SelectedTargets []string
 }
 
 type generatorOutput struct {
@@ -39,8 +39,6 @@ type generatorOutput struct {
 }
 
 var input = loadInput()
-
-
 
 func GeneratorMain(vars map[string]interface{}) {
 	output := generatorOutput{
@@ -90,7 +88,7 @@ func GeneratorMain(vars map[string]interface{}) {
 			tgt := vars[targetPath]
 			if cov, ok := tgt.(CoverageInterface); ok {
 				var selected = false
-				for _,path := range input.SelectedTargets {
+				for _, path := range input.SelectedTargets {
 					if path == targetPath {
 						selected = true
 						break
