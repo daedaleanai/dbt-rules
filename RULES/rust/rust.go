465f64e9382cc201a732cc48602594aaaec55783
Nikolai Filchenko <nf@daedalean.ai>
2023-10-26 13:23:33 +0200

    Fix hostname for rust targets inside container

--- RULES/rust/rust.go
+++ RULES/rust/rust.go
@@ -4,15 +4,20 @@ import (
 	"dbt-rules/RULES/core"
 	"fmt"
 	"io/fs"
+	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
+
+var dockerRegexp = regexp.MustCompile(`docker-([0-9a-f]*)`)
 
 type Binary struct {
 	Out       core.OutPath
 	Package   core.Path
 	Toolchain string
-	/// If target is given, compilation is done with cross instead of cargo, as cross compilation is assumed.
+
+	// If target is given, compilation is done with cross instead of cargo, as cross compilation is assumed.
 	Target string
 }
 
@@ -30,16 +35,33 @@ func (bin Binary) Build(ctx core.Context) {
 
 	cargo := "cargo"
 	target := ""
+
+	// Intelligently detect if we are using cross inside a container and if so make sure to override the HOSTNAME env variable so that
+	// it contains the docker id.
+
+	hostnameOverride := ""
 	if bin.Target != "" {
 		cargo = "cross"
 		target = fmt.Sprintf("--target=%q", bin.Target)
+
+		cgroup, err := os.ReadFile("/proc/self/cgroup")
+		if err != nil {
+			// Assume not running under docker
+			fmt.Println("Warning: could not read /proc/self/cgroup. Assuming that we are not running under docker")
+			cgroup = []byte{}
+		}
+		fmt.Println("Cgroup is: ", string(cgroup))
+		if match := dockerRegexp.FindStringSubmatch(string(cgroup)); len(match) == 2 {
+			hostnameOverride = fmt.Sprint("HOSTNAME=", match[1], " ")
+		}
+		fmt.Println("hostname override: ", hostnameOverride)
 	}
 
 	ctx.AddBuildStep(core.BuildStep{
 		Out: bin.BinLocation(),
 		Ins: bin.getInputs(),
 		// With suffix is used to turn an input path into an output path
-		Cmd: fmt.Sprintf("cd %q && %s +%s build -r --locked --bins --target-dir %q %s", bin.Package, cargo, bin.Toolchain, bin.Package.WithSuffix(""), target),
+		Cmd: fmt.Sprintf("cd %q && %s %s +%s build -r --locked --bins --target-dir %q %s", bin.Package, hostnameOverride, cargo, bin.Toolchain, bin.Package.WithSuffix(""), target),
 	})
 }
 
